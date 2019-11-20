a512b4b6b4d2d1c4071b127c62e2312b9bd45f6b
Nikolay Rozum <nickrozum@gmail.com>
2019-11-20 14:31:01 -0500

    minor changes

--- database.js
+++ database.js
@@ -2,8 +2,9 @@
 var MongoClient = require('mongodb').MongoClient;
 
 var db
+//var dbPassword = process.env.DBPASSWORD;
 
-MongoClient.connect('mongodb://qweeeq:qweeeq1@ds157493.mlab.com:57493/kolunya', (err, client) => {
+MongoClient.connect(`mongodb://qweeeq:qweeeq1@ds157493.mlab.com:57493/kolunya`, (err, client) => {
   if (err) return console.log(err);
   db = client.db('kolunya');
   console.log("DB is connected");
